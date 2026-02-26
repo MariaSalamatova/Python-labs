0651b4be8c3e71a8bbb64ec4c6e91bdcdcd10d76
MariaSalamatova <salamatovamary@gmail.com>
2026-02-26 23:05:45 +0200

    Lab 2

---
+++ lab 2/lab2.py
@@ -0,0 +1,10 @@
+#Ввід даних
+number = int(input('Enter your number:'))
+
+#Перевірка чи є число додатнім чи від'ємним
+if number > 0:
+    print('This number is positive.')
+elif number > 0:
+    print('This number is negative.')
+else:
+    print('This number is 0.')
